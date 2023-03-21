86562f791d1037d1ba51eaff446016f562e44aec
Florian Huber <florian.huber@analytical-software.de>
2023-03-21 11:19:35 +0100

    Added conftest.py.

--- autogit/tests/test_commands.py
+++ autogit/tests/test_commands.py
@@ -1,26 +1,11 @@
 import os
-import shutil
 import subprocess
-import time
 from pathlib import Path
-
-import pytest
 
 from autogit.task import AutoGitTask
 
-from .utils import ConfigBuilder, hashfile
-
-
-my_config_dir = Path("test_configs").resolve()
-my_rel_working_dir = Path("cwd", f"{time.time()}")
-
-
-assert shutil.which("git") is not None, "No git executable found, aborting."
-
-
-@pytest.fixture
-def config_builder(tmp_path):
-    return ConfigBuilder(root_dir=tmp_path)
+from .utils import ConfigBuilder
+from .conftest import my_config_dir, my_rel_working_dir
 
 
 def test_init_bare_repo(config_builder: ConfigBuilder):
--- autogit/tests/test_create_add_commit.py
+++ autogit/tests/test_create_add_commit.py
@@ -1,26 +1,9 @@
 import os
-import shutil
-import subprocess
-import time
-from pathlib import Path
-
-import pytest
 
 from autogit.task import AutoGitTask
 
 from .utils import ConfigBuilder, hashfile
-
-
-my_config_dir = Path("test_configs").resolve()
-my_rel_working_dir = Path("cwd", f"{time.time()}")
-
-
-assert shutil.which("git") is not None, "No git executable found, aborting."
-
-
-@pytest.fixture
-def config_builder(tmp_path):
-    return ConfigBuilder(root_dir=tmp_path)
+from .conftest import my_config_dir, my_rel_working_dir
 
 
 def test_create_add_commit(config_builder: ConfigBuilder):
