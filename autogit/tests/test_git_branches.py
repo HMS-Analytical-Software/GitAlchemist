90b861be47d15c4d6556a9861dbd98b246c0e033
Florian Huber <florian.huber@analytical-software.de>
2023-03-23 14:01:09 +0100

    Restructuring test file contents.

--- autogit/tests/test_git_branches.py
+++ autogit/tests/test_git_branches.py
@@ -7,6 +7,20 @@ from autogit.task import AutoGitTask
 from .utils import ConfigBuilder
 from .conftest import my_config_dir, my_rel_working_dir
 
+
+def test_init_bare_repo(config_builder: ConfigBuilder):
+    config = config_builder.create(task_name="init_bare_repo", config_dir=my_config_dir, rel_working_dir=my_rel_working_dir)
+    task = AutoGitTask.parse(config)
+    task.execute_remaining_steps()
+    os.chdir(config.bare_dir)
+    # check that bare repository is rare and that it has been cloned successfully
+    assert os.system("git rev-parse --is-bare-repository") == 0
+    os.chdir(config.repo_dir)
+    assert Path('.git').exists()
+    result = subprocess.run(['git', 'ls-remote'])
+    assert result.returncode == 0
+
+
 # tests if git mv works
 def test_git_mv(config_builder: ConfigBuilder):
     config = config_builder.create(task_name="git_branches", \
