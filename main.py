d40a519cfae2cf1c782ccfb58a0459027cf4e78a
Florian Huber <florian.huber@analytical-software.de>
2023-03-23 17:15:23 +0100

    main.py now supports command line arguments. 'task_fail' is skipped by default because it will fail. At the moment task7 is also failing because it produces a merge conflict. Still decided to not skip it because at the moment there are lots of execute_autogit_task tasks so most tasks refer to each other at some point which makes isolating errors difficult.

--- main.py
+++ main.py
@@ -2,39 +2,61 @@
 import os
 import shutil
 import time
+import argparse
 from pathlib import Path
+from typing import List
 
 from autogit.config_model import AutoGitConfig
 from autogit.task import AutoGitTask
 
-assert shutil.which("git") is not None, "No git executable found, aborting."
+def main(config_dir: Path, tasks: List, run_all = False, skip_tasks = ['task_fail']):
+    assert shutil.which("git") is not None, "No git executable found, aborting."
+    # delete everything in the working dirs if possible (does not work sometimes, 
+    # just wait and delete later)
+    shutil.rmtree('cwd', ignore_errors=True)
 
-# delete everything in the working dirs if possible (does not work sometimes, 
-# just wait and delete later)
-shutil.rmtree('cwd', ignore_errors=True)
+    config_dir = config_dir
+    working_dir = os.path.join("cwd", f"{time.time()}")
+    if run_all:
+        tasks = [str(x.name) for x in config_dir.iterdir() if x.is_dir()]
 
-working_dir = os.path.join("cwd", f"{time.time()}")
-config_dir = Path("configs") 
-my_task = "task_fail"
+    for task in tasks:
+        if task in skip_tasks:
+            continue
+        print("============== RUNNING AUTOGIT TASK", task, "==============")
+        config = AutoGitConfig(
+            task=task,
+            root_dir=os.getcwd(),
+            config_dir=config_dir, 
+            working_dir=working_dir,
+            authors={
+                'red': 'Richard Red <richard@pw-compa.ny>',
+                'blue': 'Betty Blue <betty@pw-compa.ny>',
+                'green': 'Garry Green <garry@pw-compa.ny>',
+            },
+            emails={
+                'red': 'richard@pw-compa.ny',
+                'blue': 'betty@pw-compa.ny',
+                'green': 'garry@pw-compa.ny',
+            }
+        )
 
-config = AutoGitConfig(
-    task=my_task,
-    root_dir=os.getcwd(),
-    config_dir=config_dir, 
-    working_dir=working_dir,
-    authors={
-        'red': 'Richard Red <richard@pw-compa.ny>',
-        'blue': 'Betty Blue <betty@pw-compa.ny>',
-        'green': 'Garry Green <garry@pw-compa.ny>',
-    },
-    emails={
-        'red': 'richard@pw-compa.ny',
-        'blue': 'betty@pw-compa.ny',
-        'green': 'garry@pw-compa.ny',
-    }
-)
+        task = AutoGitTask.parse(config)
+        task.execute_remaining_steps()
 
-task = AutoGitTask.parse(config)
-task.execute_remaining_steps()
 
-print("============== AUTOGIT TASKS FINISHED SUCCESSFULLY ==============")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config-dir", type=Path, default=Path("configs"), help="The directory containing the task configuration files. (default: configs)")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--task", type=str, default="task1", help="The task to run. (default: task1)")
+    group.add_argument("--run-all", action="store_true", help="Run all tasks in the configuration directory.")
+    args = parser.parse_args()
+
+    if args.run_all:
+        tasks = []
+    else:
+        tasks = [args.task]
+    
+    main(config_dir = args.config_dir, tasks = tasks, run_all = args.run_all)
+    print("============== AUTOGIT TASKS FINISHED SUCCESSFULLY ==============")
