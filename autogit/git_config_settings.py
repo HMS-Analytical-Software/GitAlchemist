a4c62a62ef090039a6cbcc6ef68531f51534a568
Richard Red richard@pw-compa.ny <richard@pw-compa.ny>
2023-06-30 09:09:17 +0200

    Updated author specification.

--- autogit/git_config_settings.py
+++ autogit/git_config_settings.py
@@ -1,13 +1,13 @@
-AUTHORS = {
-    'red': 'Richard Red <richard@pw-compa.ny>',
-    'blue': 'Betty Blue <betty@pw-compa.ny>',
-    'green': 'Garry Green <garry@pw-compa.ny>',
-}
-
 EMAILS = {
     'red': 'richard@pw-compa.ny',
     'blue': 'betty@pw-compa.ny',
     'green': 'garry@pw-compa.ny',
+}
+
+AUTHORS = {
+    'red': f"Richard Red <{EMAILS.get('red', '')}>",
+    'blue': f"Betty Blue <{EMAILS.get('blue', '')}>",
+    'green': f"Garry Green <{EMAILS.get('green', '')}>",
 }
 
 DEFAULTBRANCH = "master"
