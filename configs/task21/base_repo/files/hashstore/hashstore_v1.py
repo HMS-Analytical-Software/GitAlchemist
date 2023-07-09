640f63039a2381f41b0810bff12a18f2ce229166
Robert Bauer <Robert.Bauer@analytical-software.de>
2023-07-10 00:42:59 +0200

    added initial tasks for the git utility workshop

---
+++ configs/task21/base_repo/files/hashstore/hashstore_v1.py
@@ -0,0 +1,11 @@
+
+class HashStore():
+    
+    def get_hash(self, password: str):
+        pass
+
+    def put_hash(self, hash: str):
+        pass
+
+    def check_hash(self, hash: str) -> bool:
+        pass
---
+++ configs/task21/company_utils/files/company_logging.py
@@ -0,0 +1,3 @@
+# some code ...
+
+__VERSION__ = "0.1.16"
